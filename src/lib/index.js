81384e8ad3d9d0226e9ba14979c7a4ebe7cee0c2
NellyGonzalezB <nellygonzalezb.92@gmail.com>
2021-05-06 10:58:55 -0400

    avance de borrar y editar posts

--- src/lib/index.js
+++ src/lib/index.js
@@ -11,25 +11,80 @@ export default () => {
     const postForm = document.getElementById("post-form");
     const postContainer = document.getElementById("post-container");
 
+    let editStatus = false;
+    let id = "";
 
     const getPosts = () => db.collection("posts").get();
+    const getPost = (id) => db.collection("posts").doc(id).get();
 
     postForm.addEventListener("submit", async (e) => {
         e.preventDefault();
-
         const postIt = postForm["post"].value;
 
-        const response = await db.collection("posts").doc().set({
+        if (!editStatus){
+            const response = await db.collection("posts").doc().set({
             postIt,
         })
-
+        }else {
+           await updatePost(id, {
+                postIt,
+           })
+        }
         postForm.reset();
-        const querySnapshot = await getPosts();
-        querySnapshot.forEach(doc => {
-            postContainer.innerHTML += `<div class="card card-body mt-2 border-primary">
-            ${doc.data().postIt}
-            </div>`
-            return postContainer
+        const onGetPosts = (callback) => db.collection("posts").onSnapshot(callback);
+        const deletePosts = (id) =>db.collection ("posts").doc(id).delete();
+        const updatePost = (id, updatedPost) =>  db.collection("posts").doc(id).update(updatedPost);
+           
+        // const querySnapshot = await getPosts();
+        onGetPosts((querySnapshot) => {
+            postContainer.innerHTML = "";
+             querySnapshot.forEach(doc => {
+                const post = doc.data();
+                post.id = doc.id;
+                
+                postContainer.innerHTML += `
+                <div class="card mt-5">
+                    <div class="card-body mt-2 border-primary">
+                         ${post.postIt}
+                     </div>
+                    <div>
+                         <button class="btn btn-primary btn-delete" id="${post.id}">
+                             Delete
+                        </button>
+                        <button class="btn btn-primary btn-edit" id="${post.id}">
+                            Edit
+                        </button>
+                    </div>
+                </div>
+            `
+                const btnsDelete =document.querySelectorAll(".btn-delete");
+                btnsDelete.forEach(btn => {
+                    btn.addEventListener("click", async(e) =>{
+                        
+                        await deletePosts(e.target.id);
+                    })
+
+                });
+                const btnsEdit =document.querySelectorAll(".btn-edit");
+                btnsEdit.forEach(btn => {
+                    btn.addEventListener("click", async(e) =>{
+                        
+                        const doc = await getPost(e.target.id);
+                        const post = doc.data();
+
+                        editStatus = true;
+                        id = doc.id;
+
+                        postForm["post"].value = post.postIt;
+                        postForm["btn save-form"].innerText = "Update";
+                        console.log(doc.data());
+
+                    })
+
+                })
+
+                // return postContainer
+            })
         })
     })
 

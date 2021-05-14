b244b5327b2ae25d5c85e6f641b7ed5ab3d0ee7e
Claudia Osses <claudia.osses.mundaca@gmail.com>
2021-05-13 20:51:35 -0400

    Test index y template

--- test/routes.spec.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable no-undef */
-// import routes from '../src/routes.js'
-// describe('routes', () => {
-//   it('Deberia poder agregar un post', () => {
-//     console.log(routes('#/home'))
-//   })
-// })
